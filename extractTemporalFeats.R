eb8f8d375df0d2457cf21d205d26fafc5856fe31
Diptarup Nandi <dipatrup@aims.ac.za>
2017-03-06 17:29:08 +0200

    Desktop-Ghana update1

--- extractTemporalFeats.R
+++ extractTemporalFeats.R
@@ -1,31 +1,40 @@
 extractTF<-function(daT){
-  syllgap=.07
-  chirpgap=1
+  syllgap=.07 # sets the maximum interval between syllables (species spcific)
+  chirpgap=1  # sets the maximum interval between chirps (species spcific)
   f=44100 # sampling frequency in Hz
-  syllOnset=daT[,1]/f
-  syllOffset=daT[,3]/f
-  onset_ind=which(as.logical(daT[,2]))
-  offset_ind=which(as.logical(daT[,4]))
-  chirponset=daT[onset_ind,1]/f
-  chirpoffset=daT[offset_ind,3]/f
+  syllOnset=daT[,1]/f # syllable onsets in seconds
+  syllOffset=daT[,3]/f # syllable offsets in seconds
+  onset_ind=which(as.logical(daT[,2])) # indices of onsets
+  offset_ind=which(as.logical(daT[,4])) # indices of offsets
+  chirpOnset=daT[onset_ind,1]/f # chirp onsets in seconds
+  chirpOffset=daT[offset_ind,3]/f # chirp offsets in seconds
   
-  syLLDur=matrix(nrow=length(onset_ind),ncol=7)
-  syLLPer=matrix(nrow=length(onset_ind),ncol=7)
-  
-  for (ii in 1:length(chirponset)) {
+  syLLDur=matrix(nrow=length(onset_ind),ncol=7) # to store all syllable durations
+  syLLGap=matrix(nrow=length(onset_ind),ncol=6) # to store all the interval between syllables
+  chirpProps=matrix(nrow=length(onset_ind),ncol=3)
+  for (ii in 1:length(chirpOnset)) {
     syllpChirp=offset_ind[ii]-onset_ind[ii]+1
+    chirpProps[ii,1]=syllpChirp
+    chirpProps[ii,2]=chirpOffset[ii]-chirpOnset[ii]
+    if (offset_ind[ii] < nrow(daT)) {
+      tempCP=chirpOnset[ii+1]-chirpOnset[ii]
+      if (tempCP<chirpgap){
+        chirpProps[ii,3]=tempCP-chirpProps[ii,2] 
+      }
+    }
+      
     for (jj in 1:syllpChirp) {
       syLLDur[ii,jj]=syllOffset[onset_ind[ii]-1+jj]-syllOnset[onset_ind[ii]-1+jj]
       if (onset_ind[ii]+jj <= nrow(daT)) {
         tempSP=syllOnset[onset_ind[ii]+jj]-syllOnset[onset_ind[ii]-1+jj]
         if (tempSP<syllgap) {
-          syLLPer[ii,jj]=tempSP
+          syLLGap[ii,jj]=tempSP-syLLDur[ii,jj]
         }
         
       }
       
     }
   }
-  syLLFeats =cbind(syLLDur,syLLPer)
-  return(syLLFeats)
+  temporalFeats =cbind(syLLDur,syLLGap,chirpProps)
+  return(temporalFeats)
 }
--- temporalFeatures.R
+++ temporalFeatures.R
@@ -2,30 +2,23 @@
 ## temporal features from them and saves them into a longitudinal data form
 
 rm(list=ls())
-setwd("~/Documents/Kelsa/Individual_identification_project/scriptsR")
+setwd("~/Documents/Indiv_ID_project/scriptsR")
 
 # Calling Files
-temp = list.files(path="~/Documents/Kelsa/Individual_identification_project/Data/chirps_On_Off_set_txt_files/" ,pattern="*.txt")
-for (i in 1:length(temp)) assign(temp[i], read.table(file.path ( path= "~/Documents/Kelsa/Individual_identification_project/Data/chirps_On_Off_set_txt_files/",temp[i]),stringsAsFactors=FALSE))
+temp = list.files(path="~/Documents/Indiv_ID_project/Data/chirps_On_Off_set_txt_files/" ,pattern="*.txt")
+for (i in 1:length(temp)) assign(temp[i], read.table(file.path ( path= "~/Documents/Indiv_ID_project/Data/chirps_On_Off_set_txt_files/",temp[i]),stringsAsFactors=FALSE))
 
 
-syLLFeatures<-data.frame(indivID=character(),nIght=character(),chirpN=numeric()
+temporalFeatures<-data.frame(indivID=character(),nIght=character(),chirpN=numeric()
                          ,syl1Dur=numeric(),syl2Dur=numeric(),syl3Dur=numeric(),
                          syl4Dur=numeric(),syl5Dur=numeric(),syl6Dur=numeric(),
-                         syl7Dur=numeric(),syl12Per=numeric(),syl23Per=numeric()
-                         ,syl34Per=numeric(),syl45Per=numeric(),
-                         syl56Per=numeric(),syl67Per=numeric(),
+                         syl7Dur=numeric(),syl12Gap=numeric(),syl23Gap=numeric()
+                         ,syl34Gap=numeric(),syl45Gap=numeric(),
+                         syl56Gap=numeric(),syl67Gap=numeric(),
+                         syLpChirp=numeric(),chirpDur=numeric(),
+                         chirpGap=numeric(),
                          stringsAsFactors=FALSE)
 
-chirpFeatures<-data.frame(indivID=character(),nIght=character(),chirpN=numeric()
-                         ,syll1ChirpDur=numeric(),syl2ChirpDur=numeric(),
-                         syl3ChirpDur=numeric(),syl4ChirpDur=numeric(),
-                         syl5ChirpDur=numeric(),syl6ChirpDur=numeric(),
-                         syl7ChirpDur=numeric(),syl1ChirpPer=numeric(),
-                         syl2ChirpPer=numeric(),syl3ChirpPer=numeric(),
-                         syl4ChirpPer=numeric(),syl5ChirpPer=numeric(),
-                         syl6ChirpPer=numeric(),syl7ChirpPer=numeric(),
-                         stringsAsFactors=FALSE)
 
 
 couNT=1
@@ -33,18 +26,18 @@ for (i in 1:length(temp) ){
   data=get(temp[i])
   source("extractTemporalFeats.R")
   tempSyll=extractTF(data)
-  syLLFeatures[couNT:(couNT+nrow(tempSyll)-1),1]=substr(temp[i],1,3)
-  syLLFeatures[couNT:(couNT+nrow(tempSyll)-1),2]=substr(temp[i],4,4)
-  syLLFeatures[couNT:(couNT+nrow(tempSyll)-1),3]=1:nrow(tempSyll)
-  syLLFeatures[couNT:(couNT+nrow(tempSyll)-1),4:ncol(syLLFeatures)]=tempSyll
+  temporalFeatures[couNT:(couNT+nrow(tempSyll)-1),1]=substr(temp[i],1,3)
+  temporalFeatures[couNT:(couNT+nrow(tempSyll)-1),2]=substr(temp[i],4,4)
+  temporalFeatures[couNT:(couNT+nrow(tempSyll)-1),3]=1:nrow(tempSyll)
+  temporalFeatures[couNT:(couNT+nrow(tempSyll)-1),4:ncol(temporalFeatures)]=tempSyll
   couNT=couNT+nrow(tempSyll)
   rm(data,tempSyll)  
 }
 
-syLLFeaturesN<-syLLFeatures
+# temporalFeaturesN<-temporalFeatures
 
-colnames(syLLFeaturesN)[11:16]<-c("syl12Gap","syl23Gap","syl34Gap","syl45Gap","syl56Gap","syl67Gap")
-syLLFeaturesN[,11:16]<-syLLFeaturesN[,11:16]-syLLFeaturesN[,4:9]
+# colnames(temporalFeaturesN)[11:16]<-c("syl12Gap","syl23Gap","syl34Gap","syl45Gap","syl56Gap","syl67Gap")
+# temporalFeaturesN[,11:16]<-temporalFeaturesN[,11:16]-temporalFeaturesN[,4:9]
 
-write.csv(syLLFeaturesN,file="syllFeatures1_forModel1.csv")
+# write.csv(temporalFeaturesN,file="temporalFeatures1_forModel1.csv")
 
